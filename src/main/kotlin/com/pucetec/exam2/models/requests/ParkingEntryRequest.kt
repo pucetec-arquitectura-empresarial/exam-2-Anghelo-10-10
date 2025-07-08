9ffdf09925d416d0de621d4e2a19ce3d054ac095
Anghelo <anghelo@gmail.com>
2025-07-08 15:59:20 -0500

    Add one request

--- src/main/kotlin/com/pucetec/exam2/models/requests/ParkingEntryRequest.kt
+++ src/main/kotlin/com/pucetec/exam2/models/requests/ParkingEntryRequest.kt
@@ -1,4 +1,9 @@
 package com.pucetec.exam2.models.requests
 
-class ParkingEntryRequest {
-}
+import java.time.LocalDateTime
+
+data class ParkingEntryRequest(
+    var id: Long,
+    var assignedFloor: String,
+    var entryTime: Int,
+)
